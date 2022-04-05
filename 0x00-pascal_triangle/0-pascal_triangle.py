62364cbfb6774d951ef46d7e71dd31790c44ba1d
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-05 18:30:13 +0000

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""list of lists representing the pascal triangle"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    pascal = []
+    for i in range(n):
+        pascal.append(list(map(int,list(str(11**i)))))
+    return pascal 
