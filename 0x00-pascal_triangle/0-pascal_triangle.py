911d44c4d4dcf9e7cf2178bbbfe704ed5999d901
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-05 19:03:14 +0000

    pascal correct

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""list of lists representing the pascal triangle"""
+'''
+    list of lists representing the pascal triangle
+'''
 
 
 def pascal_triangle(n):
-    """returns a list of lists"""
-    
+    '''
+        returns a list of lists
+    '''
     pascal = []
     if n <= 0:
-        return pascal
-    
+        return []
+
     for i in range(n):
         pascal.append(list(map(int, list(str(11**i)))))
-        
+
     return pascal
