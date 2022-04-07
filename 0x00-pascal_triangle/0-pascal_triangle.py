ab1b25747c698c4211ce8cb147e3d97bf7c2af18
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-07 17:31:16 +0000

    pascal attach

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -12,7 +12,18 @@ def pascal_triangle(n):
     if type(n) is not int or n <= 0:
         return []
 
-    for i in range(n):
-        pascal.append(list(map(int, list(str(11**i)))))
-
+    if n > 0 and n <= 5:
+        for i in range(n):
+            if i < 5:
+                pascal.append(list(map(int, list(str(11**i)))))
+                
+    if n > 5:
+        for i in range(n):
+            attach = []
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    attach.append(1)
+                elif i > 0 and j > 0:
+                    attach.append(pascal[i - 1][j - 1] + pascal[i - 1][j])
+            pascal.append(attach)
     return pascal
